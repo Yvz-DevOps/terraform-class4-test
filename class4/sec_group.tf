e7c39606368f4bdbd0026265102dd66f4648409d
Yvz-DevOps <68934275+Yvz-DevOps@users.noreply.github.com>
2020-08-19 17:52:49 -0400

    Update sec_group.tf

--- class4/sec_group.tf
+++ class4/sec_group.tf
@@ -8,28 +8,7 @@ resource "aws_security_group" "asg-sec-group" {
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  
   }
   egress {
     from_port   = 0
